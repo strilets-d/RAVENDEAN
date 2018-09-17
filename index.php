01e28ae1d611cc713198e4983d68ad0f8dab0c4f
strilets-d <dima.strilets@ukr.net>
2018-09-17 19:41:09 +0300

    first design commit

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Fast with RAVEN</title>
+    <link rel="stylesheet" href="css/css.css">
+</head>
+
+
+<div class="header_line">
+<header>
+     
+ 
+
+   
+<nav id="navigation_menu">
+    
+    <ul class="menu" >
+        <li >
+             <a href="index.php" >Вхід</a> 
+
+        </li>
+        <li>
+            <a href="index.php">Реєстрація</a>
+        </li>
+        <li>
+            <a href="index.php">Додаткова інформація</a>
+        </li>
+        <li>
+            <a href="index.php" >Час роботи</a>
+        </li>
+        <li >
+            
+            <a href="index.php" onmouseover="document.getElementById('Time').style.display='block',document.getElementById('Night_Time').style.display='block';" onmouseout="document.getElementById('Time').style.display='none',document.getElementById('Night_Time').style.display='none';" style="cursor: pointer;">Доставка</a>
+<div >
+             <div id="Time"> <b>Доставка до 30 хвилин,або піцца безкоштовно!</b><br>
+             Якщо курьєр запізнився,він видасть вам карту гарантії на одну безкоштовну піццу.
+             <p>Мінімальна вартість замовлення - 200 грн. </p>
+             <div id="Night_Time" ><b>Середній час доставки 25 хв</b></div>
+         </div>
+</div>
+        </li>
+    </ul>
+    <div class="logo"></div>
+    </nav>
+
+
+    <nav class="product_line">
+<ul class="product_menu">
+       
+    <img src="/pictures/logo.png" class="pizza_men" >
+    
+
+    
+        <li>
+            <a href="index.php">Напої</a>
+        </li>
+        <li>
+            <a href="index.php">Піцца</a>
+        </li>
+        <li>
+            <a href="index.php">Соуси</a>
+        </li>
+        <li>
+            <a href="index.php">Десерти</a>
+        </li>
+       
+    </ul>
+    </nav>
+
+
+</header>
+    
+</div>
+
+
+                    
+<body>
+
+
+   
+
+
+<section class="product_list">
+    <div >
+       <p> Головне меню </p>
+
+    </div>
+</section>
+
+</body>
+
+
+</html>
--- main.php
+++ main.php
@@ -5,6 +5,6 @@
  * Date: 11.09.2018
  * Time: 18:52
  */
-echo "github is awesome";
 echo "strilets test commit ";
+echo "github is awesome";
 ?>
