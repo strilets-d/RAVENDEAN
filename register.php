3c8e71e8b18510997a345f03531c1678cf1178bd
CRfich <fichyga@ukr.net>
2018-09-26 08:15:09 +0300

    third design commit

--- register.php
+++ register.php
@@ -1,104 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <title>Fast with RAVEN</title>
-    <link rel="stylesheet" href="css/css.css">
+	<title>Реєстрація</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	
+		
+	<link href="css/style.css" rel='stylesheet' type='text/css'/>
+	<link href="css/font-awesome.css" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Hind:300,400,500,600,700&amp;subset=devanagari,latin-ext" rel="stylesheet">
+
+		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+			<script src="js/jquery.min.js"></script>
+			<script>$(document).ready(function(c) {
+			$('.alert-close').on('click', function(c){
+				$('.main-agile').fadeOut('slow', function(c){
+					$('.main-agile').remove();
+				});
+			});	  
+		});
+		</script>
 </head>
-
-
-<header>
-     
- 
-
-   
-<nav id="navigation_menu">
-    
-    <ul class="menu" >
-        <li >
-             <a href="login.php" >Вхід</a> </li>
-
-        </li>
-        <li>
-            <a href="Register.php">Реєстрація</a>
-        </li>
-        <li>
-            <a href="index.php">Додаткова інформація</a>
-        </li>
-        <li>
-            <a href="index.php" >Час роботи</a>
-        </li>
-        <li >
-            
-            <a href="index.php" onmouseover="document.getElementById('Time').style.display='block',document.getElementById('Night_Time').style.display='block';" onmouseout="document.getElementById('Time').style.display='none',document.getElementById('Night_Time').style.display='none';" style="cursor: pointer;">Доставка</a>
-<div >
-             <div id="Time"> <b>Доставка до 30 хвилин,або піцца безкоштовно!</b><br>
-             Якщо курьєр запізнився,він видасть вам карту гарантії на одну безкоштовну піццу.
-             <p>Мінімальна вартість замовлення - 200 грн. </p>
-             <div id="Night_Time" ><b>Середній час доставки 25 хв</b></div>
-         </div>
-</div>
-        </li>
-    </ul>
-    <div class="logo"></div>
-    </nav>
-
-
-    <nav class="product_line">
-<ul class="product_menu">
-       
-    <img src="/pictures/logo.png" class="pizza_men" >
-    
-
-    
-        <li>
-            <a href="index.php" class=" button ">Напої</a>
-        </li>
-        <li>
-            <a href="index.php" class=" button ">Піцца</a>
-        </li>
-        <li>
-            <a href="index.php" class=" button ">Соуси</a>
-        </li>
-        <li>
-          <a href="index.php" class=" button ">Десерти</a>
-        </li>
-       
-    </ul>
-    </nav>
-
-
-</header>
-    
-</div>
-
-
-                    
 <body>
-<form id="form_login">
+<div class="layer">
+	<h1>Вернутись на <a href="index.php">Головну</a> </h1>
+	<div class="main-agile1">
+		<div class="w3layouts-main">
+					<h2>Sign Up </h2>
+					
+						
+						
+						<span>(op)</span>
+						<h3 >Вже <a href="login.php" style="color :white; text-decoration: underline; ">зареєстровані </a>?</h3>
+						<form action="#" method="post">
+							<div class="email">
+							<input placeholder="E-Mail" name="Email" type="email" required="">
+							<span class="icons i1"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+							</div>
+							<div class="email">
+							<input placeholder="Password" name="Password" type="password" required="">
+							<span class="icons i2"><i class="fa fa-unlock" aria-hidden="true"></i></span>
+							</div>
+							<div class="email">
+							<input placeholder="Confirm Password" name="PasswordConf" type="password" required="">
+							<span class="icons i2"><i class="fa fa-unlock" aria-hidden="true"></i></span>
+							</div>
+							<input type="submit" value="Реєстрація" name="login">
 
-<div class = "login_left_side">
-    <p><b>Логін</b></p>
-    <input type="text" size="40">
-    
-    <p><b>Електрона пошта</p></b>
-    <input type="text" size="100%">
-    <p><b>Пароль</p></b>
-    <input type="text" size="40">
-    <p><b>Повторіть пароль</p></b>
-    <input type="text" size="40">
-     <br><button><a href="login.php">Вже зареєстровані</button>
-     <br><input type="submit" value="Реєстрація">
+						</form>
+		</div>
+		<div class="main-agile">
+			<div class="alert-close"> </div>
+			<div class="content-wthree">
+				
+			</div>
+		</div>
+		<div class="clear"></div>
+	</div>
+	<div class="footer-w3l">
+		<p class="agileinfo"> &copy; 2018 Sign Up and Subscribe Form. All Rights Reserved | Design by Ravendean</p>
+	</div>
 </div>
-<div class ="login_right_side">
- <p>Реклама</p>
-    </div>
-   </form>
-
-
-
 </body>
-
-
 </html>
