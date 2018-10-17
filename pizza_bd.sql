ef28ad533cefca126b3e69a7d04f0603e4682e99
strilets-d <dima.strilets@ukr.net>
2018-10-17 21:38:07 +0300

    ssss

--- pizza_bd.sql
+++ pizza_bd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Окт 09 2018 г., 09:23
+-- Время создания: Окт 17 2018 г., 21:36
 -- Версия сервера: 5.6.38
 -- Версия PHP: 5.5.38
 
@@ -68,7 +68,6 @@ INSERT INTO `Products` (`id_product`, `id_type_product`, `name_product`, `price`
 (12, 2, 'Шоколадний маффін', 25, '12.jpg'),
 (13, 2, 'Лава кейк', 48, '13.jpg'),
 (14, 2, 'Ванільний маффін', 25, '14.jpg'),
-(15, 2, 'Байтси з корицею', 45, '15.jpg'),
 (16, 3, 'Coca-Cola', 22, '16.jpg'),
 (17, 3, 'BonAqua', 20, '17.png'),
 (18, 3, 'Coca-Cola Zero', 22, '18.jpg'),
@@ -78,7 +77,15 @@ INSERT INTO `Products` (`id_product`, `id_type_product`, `name_product`, `price`
 (22, 4, 'Крильця BBQ', 95, '22.jpg'),
 (23, 4, 'Салат з куркою', 70, '23.jpg'),
 (24, 4, 'Салат з тунцем', 70, '24.jpg'),
-(25, 4, 'Картопляні дольки', 52, '25.jpg');
+(25, 4, 'Картопляні дольки', 52, '25.jpg'),
+(26, 1, 'Прованс', 127, '26.jpg'),
+(27, 1, 'Спайсі', 149, '27.jpg'),
+(28, 4, 'Хлібці з пеппероні', 76, '28.jpg'),
+(29, 4, 'Хлібці з сиром', 61, '29.jpg'),
+(30, 4, 'Крильця гострі', 95, '30.jpg'),
+(31, 3, 'Fuze Tea лимон', 29, '31.jpg'),
+(32, 3, 'Яблучний сік Rich', 39, '32.jpg'),
+(33, 3, 'Томатний сік Rich', 39, '33.jpg');
 
 -- --------------------------------------------------------
 
@@ -118,7 +125,9 @@ CREATE TABLE `usertbl` (
 --
 
 INSERT INTO `usertbl` (`id`, `email`, `password`) VALUES
-(1, 'dima.strilets@ukr.net', '1234554321');
+(1, 'dima.strilets@ukr.net', '1234554321'),
+(2, 'test@test.ua', 'test'),
+(3, 'test@test', '1234');
 
 --
 -- Индексы сохранённых таблиц
@@ -165,7 +174,7 @@ ALTER TABLE `Orders`
 -- AUTO_INCREMENT для таблицы `Products`
 --
 ALTER TABLE `Products`
-  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT для таблицы `product_type`
@@ -177,7 +186,7 @@ ALTER TABLE `product_type`
 -- AUTO_INCREMENT для таблицы `usertbl`
 --
 ALTER TABLE `usertbl`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
