d0981e4de804f691bbbe863fefe79e043748dda0
CRfich <fichyga@ukr.net>
2018-10-17 10:28:57 +0300

    alert

--- index.php
+++ index.php
@@ -9,14 +9,17 @@
     <meta charset="UTF-8">
     <title>Fast with RAVEN</title>
     <link rel="stylesheet" href="css/css.css">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css"> 
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script> 
         <link rel="shortcut icon" href="images/favico.ico" type="image/x-icon">
 <link rel="stylesheet" href="css/bootstrap-grid.css">
     <script src="cookie.js"></script>
 </head>
 <header>
 <nav id="navigation_menu">
-    
+   
     <ul class="menu" >
         <li >
              <a href="login.php" id="out" onclick="vuhid()">Вихід</a> </li>
@@ -96,6 +99,7 @@ echo "<div class='dws-wrapper'>
         </div>
         </a>
         </div>
+
         <p><i class='fas fa-exclamation-circle'></i>".$row['name_product']."<br>
         <i class='fas fa-wallet'></i>Ціна: ".$row['price']."грн.
         </p>
